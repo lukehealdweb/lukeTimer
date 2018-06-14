7746150bd895a6f93ca9ad2ca5795e1b01a343f3
lukeuni <34680196+lukeuni@users.noreply.github.com>
2018-06-14 21:42:19 +0300

    next game
    next game

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         alert("hello world!");
     });
     // Set the date we're counting down to
-var countDownDate = new Date("Jun 14, 2018 18:00:00").getTime();
+var countDownDate = new Date("Jun 15, 2018 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -36,8 +36,14 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "ENJOY THE WORLD CUP!";
   }
 }, 1000);
+    
+    $("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
     
 });
